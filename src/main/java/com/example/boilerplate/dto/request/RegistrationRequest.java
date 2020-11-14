c31d040425e55276615477b97b4c67d555ad1f7c
dimitrije97 <dimitrijesalic@yahoo.com>
2020-11-14 21:02:27 +0100

    Login and registration

---
+++ src/main/java/com/example/boilerplate/dto/request/RegistrationRequest.java
@@ -0,0 +1,17 @@
+package com.example.boilerplate.dto.request;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegistrationRequest {
+
+    @NotEmpty
+    @Email
+    private String email;
+
+    @Size(min = 3, max = 50)
+    private String password;
+}
