e542646fb87ba0c43472e4aef864f855d7676a86
dimitrije97 <dimitrijesalic@yahoo.com>
2020-12-12 01:17:23 +0100

    Minor changes

--- src/main/java/com/example/boilerplate/exception/UserAlreadyExistsException.java
+++ src/main/java/com/example/boilerplate/exception/UserAlreadyExistsException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UserAlreadyExistsException extends RuntimeException {
 
-    private static final String USER_ALREADY_EXIST = "User with email %s already exist";
+    private static final String USER_ALREADY_EXIST = "User with email %s already exists";
 
     public UserAlreadyExistsException(String email) {
         super(String.format(USER_ALREADY_EXIST, email));
